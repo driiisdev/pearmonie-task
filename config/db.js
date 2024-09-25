e2879f0f16bead746310b3b5ada5ed99d819ac37
Idris Yakub <olayinkayakub01@gmail.com>
2024-09-25 19:35:46 +0100

    ssl config

--- config/db.js
+++ config/db.js
@@ -2,17 +2,24 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME||process.env.PROD_DB_NAME,
-  process.env.DB_USERNAME||process.env.PROD_DB_USERNAME,
-  process.env.DB_PASSWORD||process.env.PROD_DB_PASSWORD,
+  process.env.DEV_DB_NAME||process.env.PROD_DB_NAME,
+  process.env.DEV_DB_NAME||process.env.PROD_DB_USERNAME,
+  process.env.DEV_DB_NAME||process.env.PROD_DB_PASSWORD,
   {
-    host: process.env.DB_HOST||process.env.PROD_DB_HOST,
+    host: process.env.DEV_DB_HOST||process.env.PROD_DB_HOST,
     dialect: 'postgres',
     logging: false,
     define: {
       underscored: false,
       freezeTableName: true,
     },
+    // comment out in dev mode
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 );
 
